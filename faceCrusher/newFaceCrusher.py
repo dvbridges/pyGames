68bf5d418fbd6265598fd34286c3ee3f34355269
David Bridges <david-bridges@hotmail.co.uk>
2016-11-16 18:01:46 +0000

    Adds music, sound effects, and death animation

--- faceCrusher/newFaceCrusher.py
+++ faceCrusher/newFaceCrusher.py
@@ -45,22 +45,34 @@ class blocks(pygame.sprite.Sprite):
 
     def update(self,dt,game):
     	if dt and self.rect.y < 640:
-    		self.rect.y += 700 * dt
+    		self.rect.y += 300 * dt
     	else:
     		self.rect.y = -100
     		self.rect.x = random.randint(0,640)
 
     	if self.rect.colliderect(game.player.rect):
-    		channela=game.explosion.play()
-    		while channela.get_busy():
-    			pygame.time.delay(1)
     		game.player.is_dead = True
+
+class texts():
+	def __init__(self):
+		self.gameOver="CRUSHED!"
+		self.red=(255,0,0)
+		self.green=(0,255,0)
+		self.blue=(0,0,255)
+
+	def setFont(self,screen):
+		fontObj=pygame.font.Font('freesansbold.ttf',64)
+		textSurfaceObj = fontObj.render(self.gameOver,True,self.red)
+		textRectObj=textSurfaceObj.get_rect()
+		textRectObj.center = (320,150)
+		screen.blit(textSurfaceObj,textRectObj)
+		pygame.display.flip()
 
 
 class Game(object):
     def main(self, screen):
         clock = pygame.time.Clock()
-
+        gameover=texts()
         background = pygame.image.load('background.png')
         pygame.mixer.music.load("ColdasSteel.mp3")
         pygame.mixer.music.play()
@@ -95,8 +107,16 @@ class Game(object):
             pygame.display.flip()
 
             if self.player.is_dead:
-            	print ('YOU DIED')
-            	return
+            	self.player.image = pygame.image.load('deadface.png')
+            	gameover.setFont(screen)
+            	channela=self.explosion.play()
+            	
+            	sprites.draw(screen)
+            	pygame.display.flip()
+            	while channela.get_busy():
+            		pygame.time.delay(100)
+            		time.sleep(2)
+            		return
 
 if __name__ == '__main__':
     pygame.init()
